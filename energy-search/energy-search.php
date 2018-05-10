bd67846f15981a43d51bcdf6222ce078de073a5f
Gamer2020 <lostatlife2020@hotmail.com>
2018-05-09 22:05:32 -0400

    Adding file.

---
+++ energy-search/energy-search.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Energy Search
+Plugin URI:	energysearch.gamer2020.net
+Description:  A Pokemon TCG plugin for Wordpress made with the Pokemon TCG API!
+Version:      0.1
+Author:       Gamer2020
+Author URI:   https://www.Gamer2020.net
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
+?>
