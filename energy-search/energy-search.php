90530e462df512ba8b6fe6b556ee8c0f230ac8f9
Gamer2020 <lostatlife2020@hotmail.com>
2018-05-12 13:50:39 -0400

    I think I did this wrong...

--- energy-search/energy-search.php
+++ energy-search/energy-search.php
@@ -13,11 +13,14 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 
 <?php
 	
-use pokemon-tcg-sdk-php\Pokemon;
+//use pokemon-tcg-sdk-php\Pokemon;
 
 /** Hooks go here*/
 /** Hook for options page.*/
 add_action( 'admin_menu', 'es_plugin_menu' );
+
+/** Shortcodes!*/
+add_shortcode('es_db_page', 'es_database_page');
 
 /** Link for options page.*/
 function es_plugin_menu() {
@@ -29,8 +32,16 @@ function es_plugin_options() {
 	if ( !current_user_can( 'administrator' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
+	screen_icon();
 	echo '<div class="wrap">';
 	echo '<p>Here is where the form would go if I actually had options.</p>';
 	echo '</div>';
 }
+?>
+
+<?php
+function es_database_page(){
+Echo 'DataBase Page here';
+}
+
 ?>
